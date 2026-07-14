e881b003f685da7ea9e68ca3f45ccd511ebcd5d9
Huy Kieu <kieuquanghuy1999@gmail.com>
2026-07-14 16:45:33 +0700

    Update kubernetes DNS url with namespace

--- api-gateway/src/main/java/com/techie/microservices/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/techie/microservices/gateway/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -23,6 +24,7 @@ public class SecurityConfig {
                         .requestMatchers(freeResourceUrls)
                         .permitAll()
                         .anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
 //                        .anyRequest().authenticated())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 //                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
